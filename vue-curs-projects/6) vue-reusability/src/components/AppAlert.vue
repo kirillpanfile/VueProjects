7b230c0d98995fb28f3027ee6c776b5b6d4fa5ac
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-04-07 10:21:58 +0300

    Created component Alert

---
+++ vue-curs-projects/6) vue-reusability/src/components/AppAlert.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="alert" :class="type">
+    <p>{{ title }}</p>
+    <p>{{ description }}</p>
+    <button class="btn" v-if="closeable" :class="type" @click="$emit('close')">
+      Close
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["close"],
+  props: {
+    title: String,
+    description: String,
+    closeable: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: "primary",
+      validator(val) {
+        return ["primary", "danger", "warning"].includes(val);
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
--- vue-curs-projects/6) vue-reusability/src/components/AppBlock.vue
+++ vue-curs-projects/6) vue-reusability/src/components/AppBlock.vue
@@ -4,8 +4,6 @@
       v-if="alert"
       description="This is an alert"
       title="Alert!"
-      type="danger"
-      closeable
       @close="alert = false"
     ></app-alert>
     <div class="card">
@@ -13,15 +11,13 @@
         {{ alert ? "Close message" : " Show message" }}
       </button>
     </div>
-    <app-block></app-block>
   </div>
 </template>
 
 <script>
-import AppAlert from "./components/AppAlert.vue";
-import AppBlock from "./components/AppBlock.vue";
+import AppAlert from "./AppAlert.vue";
 export default {
-  components: { AppAlert, AppBlock },
+  components: { AppAlert },
   data() {
     return {
       alert: false,
