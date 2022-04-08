3d68dec70d29e7af763835885b31d3c55e79355c
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-04-08 19:50:00 +0300

    first Plugin

--- vue-curs-projects/6) vue-reusability/src/App.vue
+++ vue-curs-projects/6) vue-reusability/src/App.vue
@@ -1,36 +1,16 @@
 <template>
   <div class="container">
-    <div v-if="active" class="card">
-      <h2 v-color:color.blink.hover="myColor">Talk about directives</h2>
-
-      <div class="form-control">
-        <label for="inp">Active by default</label>
-        <input v-focus type="text" id="inp" />
-      </div>
-
-      <button class="btn" @click="myColor = 'darkblue'">Change color</button>
+    <div class="card">
+      <h2>How do plugins work in Vue ?</h2>
+      <div class="btn" @click="$alert('Hello')"></div>
     </div>
   </div>
 </template>
 
 <script>
-import focusDirective from "./focusDirective";
-import colorDirective from "./colorDirective";
 export default {
-  directives: {
-    focus: focusDirective,
-    color: colorDirective,
-  },
   data() {
-    return {
-      myColor: "darkred",
-      active: true,
-    };
-  },
-  mounted() {
-    setTimeout(() => {
-      this.active = false;
-    }, 3000);
+    return {};
   },
 };
 </script>
--- vue-curs-projects/6) vue-reusability/src/main.js
+++ vue-curs-projects/6) vue-reusability/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./theme.css";
-createApp(App).mount("#app");
+import translatePlugin from "./translatePlugin";
+
+const app = createApp(App);
+
+app.use(translatePlugin, {});
+
+app.mount("#app");
