5419fedcfa7ac1554b639acfbe55a9a1a09d0cc7
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-04-09 08:13:57 +0300

    Modal Teleport

--- vue-curs-projects/6) vue-reusability/src/App.vue
+++ vue-curs-projects/6) vue-reusability/src/App.vue
@@ -2,19 +2,31 @@
   <div class="container">
     <div class="card">
       <h2>{{ $i18n("app.title") }}</h2>
-      <button @click="changeLanguage">en</button>
+      <button class="btn" @click="changeLanguage">Change Lang</button>
+      <button @click="modal = true" class="btn primary">Open Modal</button>
+
+      <teleport to="body">
+        <app-modal v-if="modal" @close="modal = false"></app-modal>
+      </teleport>
     </div>
   </div>
 </template>
 
 <script>
+import AppModal from "./components/AppModal.vue";
 export default {
   inject: ["changeI18N"],
+  components: { AppModal },
   methods: {
     changeLanguage() {
       this.changeI18N("en");
       this.$forceUpdate();
     },
+  },
+  data() {
+    return {
+      modal: false,
+    };
   },
 };
 </script>
---
+++ vue-curs-projects/6) vue-reusability/src/components/AppModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="modal-backdrop" @click="$emit('close')"></div>
+    <div class="modal">
+      <h3>
+        This is a modal header
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
+          placeat reiciendis aliquid?
+        </p>
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["close"],
+};
+</script>
+
+<style></style>
