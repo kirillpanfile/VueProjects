08933fba7ec6bfa4b73777064d930c2aec4c3d2b
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-04-25 17:11:01 +0300

    Used Animation-Gropu

--- vue-full-course/src/components/PostList.vue
+++ vue-full-course/src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3 style="text-align: center">Users List</h3>
-    <post-item
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-      @remove="$emit('remove', post)"
-    ></post-item>
+    <transition-group name="post-list">
+      <post-item
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @remove="$emit('remove', post)"
+      ></post-item>
+    </transition-group>
   </div>
   <div v-else style="text-align: center">
     <h3>No users</h3>
@@ -28,4 +30,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 1s;
+}
+.post-list-enter, .post-list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.post-list-move {
+  transition: transform 0.4s ease;
+}
+</style>
