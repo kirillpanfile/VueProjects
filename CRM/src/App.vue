10edd44f69c8a3f6cd514ca0db40de37475d6a6a
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-05-27 20:59:32 +0300

    added all components

--- CRM/src/App.vue
+++ CRM/src/App.vue
@@ -1,1 +1,28 @@
-<template></template>
+<template>
+  <component :is="layout">
+    <router-view />
+  </component>
+</template>
+
+<script>
+import EmptyLayout from "./layouts/EmptyLayout.vue";
+import MainLayout from "./layouts/MainLayout.vue";
+export default {
+  name: "app",
+  components: {
+    EmptyLayout,
+    MainLayout,
+  },
+  computed: {
+    layout() {
+      console.log(this.$route.meta);
+      return (this.$route.meta.layout || "empty") + "-layout";
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+</style>
---
+++ crm/src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+    meta: {
+      layout: "main",
+    },
+  },
+  {
+    path: "/login",
+    name: "login",
+    meta: {
+      layout: "empty",
+    },
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: {
+      layout: "main",
+    },
+    component: () => import("../views/Categories.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
